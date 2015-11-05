169ab12abc9a379effe3b84e87a4725b7931c115
LilushFess <fess2036@ya.ru>
2015-11-05 12:38:48 +0200

    init commit

---
+++ ProjectEuler/ProjectEuler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectEuler
+//
+//  Created by Yevhen Herasymenko on 11/5/15.
+//  Copyright © 2015 YevhenHerasymenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
